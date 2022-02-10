c1c71d4aa5218b6a751ad703a880f2eb516bee2f
Joseph Venable <JJVenable@gmail.com>
2022-02-09 19:45:02 -0500

    figuring out axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios'
+import {useEffect, useState } from 'react'
+// aasdf ffimportt  componentsss  from backend
 
 function App() {
+const [tools, setTools] = useState([])
+const [trucks, setTrucks] = useState([])
+const [jobs, setJobs] = useState([])
+
+useEffect(() => {
+  const getData = async () => {
+    const trucksResponse = await axios.get('http://localhost:8000/trucks/')
+    setTrucks(trucksResponse.data[0])
+  
+    const toolsResponse = await axios.get('http://localhost:8000/tools/')
+    setTools(toolsResponse.data[0])
+
+    const jobsResponse = await axios.get('http://localhost:8000/jobs/')
+    setJobs(jobsResponse.data[0])
+  }
+  getData()
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
-  );
+  
 }
 
 export default App;
