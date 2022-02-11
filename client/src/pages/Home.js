71fd312bb6951928cdd74a2f73224cb8874108df
Joseph Venable <JJVenable@gmail.com>
2022-02-11 16:30:54 -0500

    stand down friday

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
     address: '',
     scheduled: '',
     comments: '',
-    
+    job_number: ''
   })
   const handleSubmit =
     async (e) => {
@@ -30,13 +30,48 @@ function Home() {
 return (
   <div className="home-page">
     <h3>This is home</h3>
-    <h3>OMG</h3>
-    {/* <Tools /> */}
-    {/* tools??? Probably change to trucks for real usage*/}
-    {/* CREATE JOB FORM */}
-    <div className="job-form">
-          {/* IMPORT JOB FORM AS A COMPONENT */}
-    </div>
+    <h1>Put in a request/Add a task</h1>
+      <form onSubmit={handleSubmit} className='formOne' >
+        <input
+          type="text-area"
+          value={logForm.suggested_tools}
+          onChange={(e) => {
+            setLogForm({ ...logForm, suggested_tools: e.target.value })
+          }}
+          name={"suggested_tools"}
+          placeholder={"Suggested Tools"}
+        />
+        <input
+          type="text-area"
+          value={logForm.address}
+          onChange={(e) => {
+            setLogForm({ ...logForm, address: e.target.value })
+          }}
+          name={"address"}
+          placeholder={"Address"}
+        />
+        <input
+          type="text-area"
+          value={logForm.scheduled}
+          onChange={(e) => {
+            setLogForm({ ...logForm, scheduled: e.target.value })
+          }}
+          name={"scheduled"}
+          placeholder={"Scheduled Date"}
+        />
+        <input
+          type="text-area"
+          value={logForm.comments}
+          onChange={(e) => {
+            setLogForm({ ...logForm, comments: e.target.value })
+          }}
+          name={"comments"}
+          placeholder={"Comments"}
+        />
+        
+        
+        <button>Submit Request</button>
+      </form>
   </div>
 )
 }
